5ee2627693bad6d852dc2e3ee69d2bb7dda6fcf4
enotus-chang <anton.s.chegus@gmail.com>
2022-10-21 20:33:43 +0500

    Добавил оператор опциональной последовательности и альтернативные значения, если что-то пойдет не так

--- js/thumbRender.js
+++ js/thumbRender.js
@@ -1,11 +1,11 @@
 const ThumbRender = function(template, item) {
   const newPost = template.cloneNode(true);
   const picture = newPost.querySelector('.picture__img');
-  picture.src = item.url;
+  picture.src = item?.url || 'no-photo.jpg';
   const likes = newPost.querySelector('.picture__likes');
-  likes.textContent = item.likes;
+  likes.textContent = item?.likes || '0';
   const comments = newPost.querySelector('.picture__comments');
-  comments.textContent = item.comments;
+  comments.textContent = item?.comments || '0';
   return newPost;
 };
 
