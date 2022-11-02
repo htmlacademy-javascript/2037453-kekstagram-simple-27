15ce05bdc5a85b7245e23cb704d89d39e1ec2620
enotus-chang <anton.s.chegus@gmail.com>
2022-11-02 10:37:14 +0500

    Вынес параметры в модуль settings.js

--- js/settings.js
+++ js/settings.js
@@ -21,4 +21,48 @@ const SENTENCES = [
 
 const POST_COUNT = 25;
 
-export {SENTENCES, POST_COUNT};
+const IMG_PARAMS = {
+  imgScale: 1,
+  imgScaleMin: .25,
+  imgScaleMax: 1,
+  imgScaleStep: .25,
+  imgEffects: {
+    chrome: {
+      name: 'grayscale',
+      min: 0,
+      max: 1,
+      step: .1,
+      unit: '',
+    },
+    sepia: {
+      name: 'sepia',
+      min: 0,
+      max: 1,
+      step: .1,
+      unit: '',
+    },
+    marvin: {
+      name: 'invert',
+      min: 0,
+      max: 100,
+      step: 1,
+      unit: '%',
+    },
+    phobos: {
+      name: 'blur',
+      min: 0,
+      max: 3,
+      step: .1,
+      unit: 'px',
+    },
+    heat: {
+      name: 'brightness',
+      min: 1,
+      max: 3,
+      step: .1,
+      unit: '',
+    },
+  },
+};
+
+export {SENTENCES, POST_COUNT, IMG_PARAMS};
