41821a490e28d3f9df0970bb18ac80e0e1980b18
enotus-chang <anton.s.chegus@gmail.com>
2022-11-16 00:09:11 +0500

    Убрал ссылки с превью картинок

--- js/thumbRender.js
+++ js/thumbRender.js
@@ -6,7 +6,6 @@ const thumbRender = function (template, {
 }) {
   const newPost = template.cloneNode(true);
   const newPostLink = newPost.querySelector('.picture');
-  newPostLink.href = url;
   const newPostPicture = newPost.querySelector('.picture__img');
   newPostPicture.src = url;
   newPostPicture.alt = description;
