8a1d3916983d89b0bfc959113cfc8bbe230809c9
enotus-chang <anton.s.chegus@gmail.com>
2022-11-16 01:20:01 +0500

    Вынес установку параметров модалок в отдельные функции

--- js/imgUpload.js
+++ js/imgUpload.js
@@ -3,10 +3,45 @@ import {modalHandler} from './util.js';
 import {imgEditor} from './imgEditor.js';
 
 const container = document.querySelector('.pictures');
+
+const setSuccessModal = function() {
+  modalHandler.modal = {
+    template: '#success',
+    selector: '.success',
+    container: container,
+  };
+  modalHandler.modalTitle = {
+    selector: '.success__title',
+    text: 'Изображение успешно загружено',
+  };
+  modalHandler.modalButton = {
+    show: true,
+    selector: '.success__button',
+    text: 'Круто!',
+  };
+}
+
+const setErrorModal = function(err) {
+  modalHandler.modal = {
+    template: '#error',
+    selector: '.error',
+    container: container,
+  };
+  modalHandler.modalTitle = {
+    selector: '.error__title',
+    text: `${err} Ошибка загрузки файла`,
+  };
+  modalHandler.modalButton = {
+    show: true,
+    selector: '.error__button',
+    text: 'Попробовать ещё раз',
+  };
+}
+
 const imgUpload = function () {
+  imgEditor.init();
   const form = document.querySelector('#upload-select-image');
   const pristine = new Pristine(form);
-  imgEditor.init();
   form.onsubmit = function (evt) {
     const submitBtn = form.querySelector('#upload-submit');
     const formData = new FormData(form);
@@ -27,40 +62,14 @@ const imgUpload = function () {
         imgEditor.reset();
         form.reset();
         pristine.destroy();
-        modalHandler.modal = {
-          template: '#success',
-          selector: '.success',
-          container: container,
-        };
-        modalHandler.modalTitle = {
-          selector: '.success__title',
-          text: 'Изображение успешно загружено',
-        };
-        modalHandler.modalButton = {
-          show: true,
-          selector: '.success__button',
-          text: 'Круто!',
-        };
+        setSuccessModal();
       })
       .catch((err) => {
-        modalHandler.modal = {
-          template: '#error',
-          selector: '.error',
-          container: container,
-        };
-        modalHandler.modalTitle = {
-          selector: '.error__title',
-          text: `${err} Ошибка загрузки файла`,
-        };
-        modalHandler.modalButton = {
-          show: true,
-          selector: '.error__button',
-          text: 'Попробовать ещё раз',
-        };
+        setErrorModal(err);
       })
       .finally(() => {
-        modalHandler.open();
         submitBtn.disabled = false;
+        modalHandler.open();
       });
   };
 };
